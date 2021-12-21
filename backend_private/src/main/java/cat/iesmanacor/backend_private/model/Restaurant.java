c2725b9d1e80518e218e04fe54cba6563999b32c
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-21 11:28:07 +0100

    mallorca done it

---
+++ backend_private/src/main/java/cat/iesmanacor/backend_private/model/Restaurant.java
@@ -0,0 +1,18 @@
+package cat.iesmanacor.backend_private.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="restaurante")
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String population;
+    private String type;
+    private int categoryPrice;
+}
