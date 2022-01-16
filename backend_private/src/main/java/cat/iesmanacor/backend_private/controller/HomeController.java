c0ebc09e773f79648612570b78a664931cc074fb
Andres Garcia Bauza <agarcia15183@alumnes.iesmanacor.cat>
2022-01-16 13:34:53 +0100

    Lista de restaurantes

--- backend_private/src/main/java/cat/iesmanacor/backend_private/controller/HomeController.java
+++ backend_private/src/main/java/cat/iesmanacor/backend_private/controller/HomeController.java
@@ -1,12 +1,20 @@
 package cat.iesmanacor.backend_private.controller;
 
+import cat.iesmanacor.backend_private.services.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
     @GetMapping("/home")
-    public String getHome(){
+    public String getHome(ModelMap model){
+        model.addAttribute("restaurantes",restaurantService.findAllRestaurants());
         return "principalPage";
     }
 
