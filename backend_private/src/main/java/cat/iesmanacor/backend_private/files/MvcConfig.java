97ff59ee2b0dcc898fd0acd5696eb8a044735d18
Andres Garcia Bauza <agarcia15183@alumnes.iesmanacor.cat>
2022-01-25 10:46:21 +0100

    aplication propierties active

--- backend_private/src/main/java/cat/iesmanacor/backend_private/files/MvcConfig.java
+++ backend_private/src/main/java/cat/iesmanacor/backend_private/files/MvcConfig.java
@@ -1,17 +1,20 @@
 package cat.iesmanacor.backend_private.files;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Autowired
+    Environment environment;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println(environment.getProperty("img.file.destiny"));
         exposeDirectory("restaurantes-photos",registry);
     }
 
