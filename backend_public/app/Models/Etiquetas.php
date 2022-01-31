32b8f3b7c903585165a16f7e3d2257507139c6a2
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2022-01-31 09:06:35 +0100

    Modelos base de datos laravel

---
+++ backend_public/app/Models/Etiquetas.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Etiquetas extends Model
+{
+    use HasFactory;
+
+    protected $table = "etiquetas";
+    public $timestamps = false;
+}
---
+++ backend_public/app/Models/Membresia.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Membresia extends Model
+{
+    use HasFactory;
+
+    protected $table = "membresia";
+    public $timestamps = false;
+}
