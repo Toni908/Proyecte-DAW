bda11b59e547e9c8296629a8a98e77efbf83bf13
Andres Garcia Bauza <agarcia15183@alumnes.iesmanacor.cat>
2022-02-01 11:41:49 +0100

    restaurant changes

--- backend_private/src/main/java/cat/iesmanacor/backend_private/controller/RestaurantControllerImpl.java
+++ backend_private/src/main/java/cat/iesmanacor/backend_private/controller/RestaurantControllerImpl.java
@@ -220,7 +220,6 @@ public class RestaurantControllerImpl {
                 updateRestaurant(restaurant.get());
                 if (validation) {
                     emailService.sendSimpleMessage("agarcia15183@alumnes.iesmanacor.cat", "Validacion " + restaurant.get().getNombre(), "El restaurante " + restaurant.get().getNombre() + " acaba de ser validado por un administrador, ahora mismo ya puede ser visible para todos los usuarios, para realizar algun cambio por si aun no lo has hecho http//localhost:8080/restaurant/update/" + restaurant.get().getId_restaurante() + " , para mas info visite a la pestaña de preguntas.");
-//                emailService.sendSimpleMessage(restaurant.get().getUseracount().getCorreo(),"Validacion "+restaurant.get().getNombre(),"El restaurante "+restaurant.get().getNombre()+" acaba de ser validado por un administrador, ahora mismo ya puede ser visible para todos los usuarios, para realizar algun cambio por si aun no lo has hecho http//localhost:8080/restaurant/update/"+restaurant.get().getId_restaurante()+" , para mas info visite a la pestaña de preguntas.");
                 }
             }
         }
