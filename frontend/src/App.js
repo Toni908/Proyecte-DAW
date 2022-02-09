db587ec9d134f573dcbe08a6b4aea252e1cba34f
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2022-02-09 11:29:30 +0100

    ojh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,24 +14,23 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Nav className="p-3 bg-dark text-white">
+          <Nav class="p-3 mb-3 border-bottom">
             <div class="container">
               <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-xl-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none d-lg-none">
-                  <img src="./img/trobalot.png" alt="logo trobalo"></img>
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
                 </a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                  <li><Link to="/" class="nav-link px-2 text-white">Home</Link></li>
-                  <li><Link to="/search" class="nav-link px-2 text-white">Buscador Avanzado</Link></li>
-                  <li><Link to="#" class="nav-link px-2 text-white">Pricing</Link></li>
-                  <li><Link to="#" class="nav-link px-2 text-white">FAQs</Link></li>
-                  <li><Link to="#" class="nav-link px-2 text-white">Sobre Nosotros</Link></li>
+                <li><Link to="/" class="nav-link px-2 text-black">Home</Link></li>
+                  <li><Link to="/search" class="nav-link px-2 text-black">Buscador Avanzado</Link></li>
+                  <li><Link to="#" class="nav-link px-2 text-black">Pricing</Link></li>
+                  <li><Link to="#" class="nav-link px-2 text-black">FAQs</Link></li>
+                  <li><Link to="#" class="nav-link px-2 text-black">Sobre Nosotros</Link></li>
                 </ul>
 
-                <div class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                  <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search"/>
-                </div>
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                  <input type="search" class="form-control" placeholder="Search..." aria-label="Search"/>
+                </form>
 
                 <div class="text-end">
                   <Button color='light' outline className="me-2">Acceder</Button>
