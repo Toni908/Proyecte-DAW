9691b14d1887b761c566f5deb4e2510ab79f861d
Andres Garcia Bauza <agarcia15183@alumnes.iesmanacor.cat>
2022-02-15 17:59:38 +0100

    solution etiquetas y mensajes

--- backend_private/src/main/resources/static/js/etiquetas.js
+++ backend_private/src/main/resources/static/js/etiquetas.js
@@ -8,6 +8,20 @@ const changeMunicipioLocalidad = $("#localidadMunicipioChange");
 const municipios = $("#municipio");
 const error = $("#error");
 
+function hideMessagesEtiqueta() {
+    setTimeout(function (){error.hide()},3500);
+    setTimeout(function (){error.hide()},3500);
+}
+function showMessagesEtiqueta() {
+    error.show();
+    error.show();
+}
+
+function flashMessagesEtiqueta() {
+    showMessagesEtiqueta();
+    hideMessagesEtiqueta();
+}
+
 function searchEtiquetas(etiquetaSearched) {
     $.getJSON( "/get/etiquetas/admin/json", function( data ) {
         let length = 0;
@@ -35,45 +49,44 @@ function addEtiqueta(value) {
             if (isEtiquetaAdded(selectText.val())===false) {
                 if (selectText.val().length<15) {
                     if (selectText.val().length>2) {
-                        $("#etiquetas_box").append("<div onclick='deleteEtiqueta(this)' class='text-center d-flex flex-row flex-wrap' ><i class='bi bi-tag-fill'><input name='etiquetas' class='border-0 text-center' style='background-color: lightgray; width: 8vw; height: auto' value='" + selectText.val() + "' readonly><input class='align-self-center rounded-circle fw-bold border-0 bg-danger text-white pt-0' style='font-size: 10px; width: 20px; height: 20px' type='button' value='X'></div>");
+                        $("#etiquetas_box").append("<div onclick='deleteEtiqueta(this)' class='ps-3 pe-3 text-center d-flex flex-row flex-wrap' style='border-radius: 15px; border: lightgrey 1px solid'><input name='etiquetas' class='border-0 text-center' style='border-radius: 15px; border: lightgrey 1px solid; width: 8vw; height: auto' value='" + selectText.val() + "' readonly><div class='align-self-center rounded-circle fw-bold border-0 bg-danger text-white pt-0' style='font-size: 10px; width: 20px; height: 20px'>X</div>");
                         etiquetasAdded.push(selectText.val());
                         numEtiquetas--;
                     } else {
                         error.text(traductions.short);
-                        flashMessages();
+                        flashMessagesEtiqueta();
                     }
                 } else {
                     error.text(traductions.long);
-                    flashMessages();
+                    flashMessagesEtiqueta();
                 }
             } else {
                 error.text(traductions.added);
-                flashMessages();
-                setTimeout(function() {$("#error").hide()}, 1200);
+                flashMessagesEtiqueta();
             }
         } else {
             if (isEtiquetaAdded(value)===false) {
                 if (value.length<15) {
                     if (value.length>2) {
-                        $("#etiquetas_box").append("<div onclick='deleteEtiqueta(this)' class='text-center d-flex flex-row flex-wrap'><i class='bi bi-tag-fill'></i><input name='etiquetas' class='border-0 text-center' style='background-color: lightgray; width: 8vw; height: auto' value='" + value + "' readonly><input class='align-self-center rounded-circle fw-bold border-0 bg-danger text-white pt-0' style='font-size: 10px; width: 20px; height: 20px' type='button' value='X'></div>");
+                        $("#etiquetas_box").append("<div onclick='deleteEtiqueta(this)' class='ps-3 pe-3 text-center d-flex flex-row flex-wrap' style='border-radius: 10px; border: lightgrey 1px solid'><input name='etiquetas' class='border-0 text-center' style='border-radius: 15px; border: lightgrey 1px solid; width: 8vw; height: auto' value='" + value + "' readonly><div class='align-self-center rounded-circle fw-bold border-0 bg-danger text-white pt-0' style='font-size: 10px; width: 20px; height: 20px'>X</div>");
                         etiquetasAdded.push(value);
                         numEtiquetas--;
                     } else {
                         error.text(traductions.short);
-                        flashMessages();
+                        flashMessagesEtiqueta();
                     }
                 } else {
                     error.text(traductions.long);
-                    flashMessages();
+                    flashMessagesEtiqueta();
                 }
             } else {
                 error.text(traductions.added);
-                flashMessages();
+                flashMessagesEtiqueta();
             }
         }
     } else {
         error.text(traductions.max);
-        flashMessages();
+        flashMessagesEtiqueta();
     }
 }
 
