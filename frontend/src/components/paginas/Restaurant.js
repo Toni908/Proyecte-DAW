49cce49866027dc19accc3dff003feda81adfaf7
AndresIESManacor <agarcia15183@alumnes.iesmanacor.cat>
2022-02-23 20:59:34 +0100

    Gallery functional but warning everywhere

--- frontend/src/components/paginas/Restaurant.js
+++ frontend/src/components/paginas/Restaurant.js
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import axios from "axios";
-import "./restaurant.css";
 import Loading from "../components_interns/Loading";
+import "photoswipe/dist/photoswipe.css";
+import "photoswipe/dist/default-skin/default-skin.css";
+import { Gallery, Item } from "react-photoswipe-gallery";
+import "./restaurant.css";
+
 
 class Restaurant extends Component {
     constructor() {
@@ -52,7 +56,7 @@ class Restaurant extends Component {
         return(
             <section>
                 <div className={"d-flex flex-row justify-content-center w-100"}>
-                    <div className={"d-flex flex-column main-width-restaurant"}>
+                    <div className={"d-flex flex-column main-width-restaurant m-0"}>
                         <section className={"d-flex flex-column"}>
                             <h3 className={"w-100"}><i className="bi bi-building pe-3"/>{restaurant.nombre}</h3>
                             <div className={"d-flex flex-row"}>
@@ -63,9 +67,140 @@ class Restaurant extends Component {
                                 ·
                             </div>
                         </section>
+                        <section className={"d-flex flex-column pt-2"}>
+                            <div className={"w-100"}>
+                                <div className={"row p-0 m-0"}>
+                                    <div className={"col-lg-6 col-12 p-0 m-0"}>
+                                        <Gallery>
+                                            {restaurant.imgs!==undefined && <Item
+                                                original={process.env.REACT_APP_API_URL+"/image/"+restaurant.imgs[0].id_restaurante+"/"+restaurant.imgs[0].url}
+                                                thumbnail={process.env.REACT_APP_API_URL+"/image/"+restaurant.imgs[0].id_restaurante+"/"+restaurant.imgs[0].url}
+                                                width="1024"
+                                                height="768"
+                                            >
+                                                {({ ref, open }) => (
+                                                    <img className={"border-item1 p-0 px-2 px-lg-0 width-image1"} height={"500"}
+                                                         ref={ref}
+                                                         onClick={open}
+                                                         src={process.env.REACT_APP_API_URL+"/image/"+restaurant.imgs[0].id_restaurante+"/"+restaurant.imgs[0].url}
+                                                         alt={"is a item"}
+                                                    />
+                                                )}
+                                            </Item>}
+                                        </Gallery>
+                                    </div>
+                                    <div className={"col-lg-6 col-12 row p-0 m-0"}>
+                                        <Gallery>
+                                            {restaurant.imgs !== undefined &&
+                                            <section className={"p-0"}>
+                                                {restaurant.imgs.map(function(img, key) {
+                                                    if (img.id_img===restaurant.imgs[0].id_img) {
+                                                        return(<div key={key} id={"notImage"}/>)
+                                                    } else {
+                                                        if (key===1) {
+                                                            return (
+                                                                <Item key={key}
+                                                                      original={process.env.REACT_APP_API_URL + "/image/" + img.id_restaurante + "/" + img.url}
+                                                                      thumbnail={process.env.REACT_APP_API_URL + "/image/" + img.id_restaurante + "/" + img.url}
+                                                                      width="1024"
+                                                                      height="768"
+                                                                >
+                                                                    {({ref, open}) => (
+                                                                        <img key={key}
+                                                                             className={"pb-lg-1 px-lg-2 p-2 p-lg-0 width-image2"}
+                                                                             ref={ref}
+                                                                             onClick={open}
+                                                                             src={process.env.REACT_APP_API_URL + "/image/" + img.id_restaurante + "/" + img.url}
+                                                                             alt={"is a item"}
+                                                                        />
+                                                                    )}
+                                                                </Item>
+                                                            )
+                                                        } else if (key===2) {
+                                                            return (
+                                                                <Item key={key}
+                                                                      original={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                      thumbnail={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                      width="1024"
+                                                                      height="768"
+                                                                >
+                                                                    {({ref, open}) => (
+                                                                        <img key={key}
+                                                                             className={"pb-lg-1 p-2 border-right-top p-lg-0 px-lg-1 width-image2"}
+                                                                             ref={ref}
+                                                                             onClick={open}
+                                                                             src={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                             alt={"is a item"}
+                                                                        />
+                                                                    )}
+                                                                </Item>
+                                                            )
+                                                        } else if (key===3) {
+                                                            return (
+                                                                <Item key={key}
+                                                                      original={process.env.REACT_APP_API_URL + "/image/" + img.id_restaurante + "/" + img.url}
+                                                                      thumbnail={process.env.REACT_APP_API_URL + "/image/" + img.id_restaurante + "/" + img.url}
+                                                                      width="1024"
+                                                                      height="768"
+                                                                >
+                                                                    {({ref, open}) => (
+                                                                        <img key={key}
+                                                                             className={"pt-lg-1 p-2 px-lg-2 p-lg-0 width-image2"}
+                                                                             ref={ref}
+                                                                             onClick={open}
+                                                                             src={process.env.REACT_APP_API_URL + "/image/" + img.id_restaurante + "/" + img.url}
+                                                                             alt={"is a item"}
+                                                                        />
+                                                                    )}
+                                                                </Item>
+                                                            )
+                                                        } else if (key===4) {
+                                                            return (
+                                                                <Item key={key}
+                                                                      original={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                      thumbnail={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                      width="1024"
+                                                                      height="768"
+                                                                >
+                                                                    {({ref, open}) => (
+                                                                        <img key={key}
+                                                                             className={"border-right-bottom p-lg-0 pt-lg-1 p-2 px-lg-1 width-image2"}
+                                                                             ref={ref}
+                                                                             onClick={open}
+                                                                             src={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                             alt={"is a item"}
+                                                                        />
+                                                                    )}
+                                                                </Item>
+                                                            )
+                                                        } else {
+                                                            return (
+                                                                <Item key={key}
+                                                                      original={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                      thumbnail={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                      width="1024"
+                                                                      height="768"
+                                                                >
+                                                                    {({ref, open}) => (
+                                                                        <img key={key}
+                                                                             ref={ref}
+                                                                             className={"px-lg-1 pt-lg-1 p-lg-0 p-2 width-image2"}
+                                                                             onClick={open}
+                                                                             src={process.env.REACT_APP_API_URL+"/image/"+img.id_restaurante+"/"+img.url}
+                                                                             alt={"is a item"}
+                                                                        />
+                                                                    )}
+                                                                </Item>
+                                                            )
+                                                        }
 
-                        <section>
-                            //IMG
+                                                    }
+                                                })}
+                                            </section>}
+                                        </Gallery>
+                                    </div>
+                                </div>
+                            </div>
                         </section>
                     </div>
                 </div>
