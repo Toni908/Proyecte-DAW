f25be7ad93f55754d14bc67a3798c233907942d0
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2022-02-28 19:00:02 +0100

    columnas filtros

--- frontend/src/components/components_interns/Resultados.js
+++ frontend/src/components/components_interns/Resultados.js
@@ -49,7 +49,7 @@ class Resultados extends Component {
             <div className="d-flex flex-row flex-wrap">
               {restaurants.map(function(item, key) {
                         return (
-                            <CardRestaurant key={key} restaurant={item} localidad={item.localidad}/>
+                            <CardRestaurant className={"col-3 border-0 mb-2 px-2"} key={key} restaurant={item} localidad={item.localidad}/>
                         )
                     })}
             </div>}
