db9533f64be6b8cbc608317d02fc5dbad3413e7b
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2022-03-02 18:31:09 +0100

    s

--- frontend/src/components/components_interns/Resultados.js
+++ frontend/src/components/components_interns/Resultados.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardRestaurant from "./CardRestaurant";
-import ListRestaurant from "./ListRestaurant";
 import "./list_restaurants.css"
 
 class Resultados extends Component {
@@ -15,22 +14,6 @@ class Resultados extends Component {
     }
 
     componentDidMount(){
-    /*  let ip = process.env.REACT_APP_API_URL;
-      this.setState({ isLoading: true });
-
-      const request1 = axios.get(ip+"/restaurants");
-      const request2 = axios.get(ip+"/economic");
-      const request3 = axios.get(ip+"/capitales");
-
-      axios.all([request1, request2,request3]).then(axios.spread((...responses) => this.setState({
-          BestRestaurants: responses[0].data,
-          EconomicRestaurants: responses[1].data,
-          CapitalesRestaurants: responses[2].data,
-          isLoading: false
-      }))).catch(error => this.setState({
-          error: error,
-      }))*/
-
       this.setState({
         restaurants: this.props.restaurants
       });
