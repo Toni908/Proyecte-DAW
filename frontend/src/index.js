9b4ca4a09e5699c84e3f59448d2526399d6124de
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2022-03-03 11:51:42 +0100

    regist worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +17,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorker.unregister();
+
+// Para registrar nuestro serviceWorker cambiar por:
+serviceWorker.register();
