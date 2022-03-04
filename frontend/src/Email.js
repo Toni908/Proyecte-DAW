e300d1772644aca9776d021d156024b396f894bf
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2022-03-04 08:35:38 +0100

    email good

--- frontend/src/Email.js
+++ frontend/src/Email.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
-import emailjs from '@emailjs/browser';
+import $ from 'jquery';
 
 class Email extends Component {
 
-    sendEmail(e) {
-        e.preventDefault();
-    
-        emailjs.sendForm('service_1oq8hpi', 'template_d3g3uye', e.target, '8MM4-J8FO99oHPBq9')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
+    sendEmail() {
+        var data = {
+            service_id: 'service_1oq8hpi',
+            template_id: 'template_d3g3uye',
+            user_id: '8MM4-J8FO99oHPBq9',
+            template_params: {
+                'reply_to': 'tlovesc908@gmail.com',
+                'ruta': 'ruta'
+            }
+        };
+         
+        $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json'
+        }).done(function() {
+            alert('Your mail is sent!');
+        }).fail(function(error) {
+            alert('Oops... ' + JSON.stringify(error));
+        });
       }
 
     render() {
     return(
-        <div className='' >
-            <form onSubmit={this.sendEmail}>
-                Email:
-                <input type="text" name="reply_to"></input>
-                Mensaje:
+        /*<div className='' >
+            <form onSubmit={this.sendEmail} onLoad={this.onSubmit}>
+                <input type="text" name="reply_to" value="tlovesc908@gmail.com"></input>
                 <input type="text" name="ruta"></input>
                 <input type="submit"></input>
             </form>
+        </div>*/
+        <div>
+            <button onClick={this.sendEmail}>click</button>
         </div>
         );
     }
