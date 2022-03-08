f5247e5aa0180b9bf2bc5bf1795f879c7d368384
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2022-03-08 10:09:46 +0100

    Revert "Verificacion codigo html y ver restaurante hecho"
    This reverts commit 3feb83d08e980a6c1b43d1fb88910677b295c5a2.

--- backend_private/src/main/java/cat/iesmanacor/backend_private/entities/Restaurant.java
+++ backend_private/src/main/java/cat/iesmanacor/backend_private/entities/Restaurant.java
@@ -1,6 +1,5 @@
 package cat.iesmanacor.backend_private.entities;
 
-import cat.iesmanacor.backend_private.controller.CardController;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.ToString;
@@ -77,11 +76,6 @@ public class Restaurant {
     public String getPhotosImagePath() {
         if (id_restaurante == null) return null;
         return "/restaurantes-photos/"+ id_restaurante + "/";
-    }
-
-    public String getUrl() {
-        if (id_restaurante == null) return null;
-        return "https://"+CardController.urlw+"/restaurant/"+ id_restaurante;
     }
 
     public Restaurant() {
